f7bfc98191417d2ed070dc17e9345bacd39dc989
John <masterjohn553@gmail.com>
2024-05-04 15:41:31 +0300

    added reddit_adapter.py file which contains RedditAdapter class to handle actions on reddit

---
+++ consts.py
@@ -0,0 +1,17 @@
+GET_TOP_STORY_GENERIC_COOKIES = {"edgebucket": "whqdLUny3GkZu5z94Z",
+                                 "loid": "000000000znlxhg8jx.2.1714819605801.Z0FBQUFBQm1OaElWWEhMTDQ2bEZ0cXF4ODlQa2xFNXRsUVhzZ3B5WC1CSzI4RlVuOFE5SjRwVEl6cGxIMnNFOXJNempZT0pQQXVzcU5Kd0VINnFMdWVWcUdiakdkZnU0TDBsTkt6b24tb0Vkc0FsaS1jLVo2Ym5meHg1Z25vSzZyN3dycTVjWlgxRXk"}
+GET_TOP_STORY_GENERIC_HEADERS = {"Sec-Ch-Ua": "\"Chromium\";v=\"123\", \"Not:A-Brand\";v=\"8\"",
+                                 "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Windows\"",
+                                 "Upgrade-Insecure-Requests": "1",
+                                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.122 Safari/537.36",
+                                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                                 "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
+                                 "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate, br",
+                                 "Accept-Language": "en-US,en;q=0.9", "Priority": "u=0, i"}
+
+KEY_TO_FIND_TOP_STORY_BODY_HTML_TAG = 'data-post-click-location="text-body"'
+KEY_TO_FIND_TOP_STORY_START_OF_BODY = '<p>'
+KEY_TO_FIND_TOP_STORY_END_OF_BODY = '</div>'
+KEY_TO_FIND_TOP_STORY_TITLE_HTML_TAG = f'slot="title"'
+KEY_TO_FIND_TOP_STORY_START_OF_TITLE = '>'
+KEY_TO_FIND_TOP_STORY_END_OF_TITLE = '<'
---
+++ src/reddit_adapter.py
@@ -0,0 +1,35 @@
+import requests
+
+from consts import *
+from typing import Tuple
+
+
+class RedditAdapter:
+
+    def _get_top_story_title(self, http_response_text: str):
+        index = http_response_text.find(KEY_TO_FIND_TOP_STORY_TITLE_HTML_TAG) + len(
+            KEY_TO_FIND_TOP_STORY_TITLE_HTML_TAG)
+        http_response_text = http_response_text[index:]
+        story_title_start_index = http_response_text.find(KEY_TO_FIND_TOP_STORY_START_OF_TITLE) + len(
+            KEY_TO_FIND_TOP_STORY_START_OF_TITLE)
+        story_title_end_index = http_response_text.find(KEY_TO_FIND_TOP_STORY_END_OF_TITLE)
+        top_story_title = http_response_text[story_title_start_index:story_title_end_index]
+        return top_story_title
+
+    def _get_top_story_body(self, http_response_text: str):
+        index = http_response_text.find(KEY_TO_FIND_TOP_STORY_BODY_HTML_TAG)
+        http_response_text = http_response_text[index:]
+        story_body_start_index = http_response_text.find(KEY_TO_FIND_TOP_STORY_START_OF_BODY)
+        story_body_end_index = http_response_text.find(KEY_TO_FIND_TOP_STORY_END_OF_BODY) - len(
+            KEY_TO_FIND_TOP_STORY_END_OF_BODY)
+        top_story_body_text = http_response_text[story_body_start_index:story_body_end_index]
+        return top_story_body_text
+
+    def get_top_story(self, subreddit_name: str) -> Tuple[str, str]:
+        subreddit_url = f"https://www.reddit.com:443/r/{subreddit_name}/top/"
+        response = requests.get(subreddit_url, headers=GET_TOP_STORY_GENERIC_HEADERS,
+                                cookies=GET_TOP_STORY_GENERIC_COOKIES)
+        response_text = response.text
+        top_story_title = self._get_top_story_title(response_text)
+        top_story_body = self._get_top_story_body(response_text)
+        return (top_story_title, top_story_body)
