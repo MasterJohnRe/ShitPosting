2b46235b4281a6a093f714d71f52e31f4450c135
John <masterjohn553@gmail.com>
2024-06-13 15:01:10 +0300

    added logging and exception handling which was neccesary for logging correctly

---
+++ src/exceptions.py
@@ -0,0 +1,4 @@
+class DownloadFileFromS3Error(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
---
+++ src/log_config.py
@@ -0,0 +1,10 @@
+# log_config.py
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        filename='./logs/shitposting_logs.log',
+        encoding='utf-8',
+        format='%(asctime)s %(levelname)s: %(message)s',
+        level=logging.DEBUG
+    )
